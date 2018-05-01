a950163750694e7b57c8e4f1a181b606a4f3af32
unholydeath785 <ivansonofcoul@gmail.com>
2018-05-01 11:26:24 -0700

    finished

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+let filePath = path.join('.', 'dogs.txt');
+
+router.get('/', (req, res, next) => {
+	let readStream = fs.createReadStream(filePath);
+	let data = [];
+	readStream.on('data', (text) => {
+		let names = text.toString('utf8').split('\n');
+		for (let i = 0; i < names.length; i++) {
+			if (names[i].length > 0) {
+				data.push(names[i]);
+			}
+		}
+	});
+	readStream.on('end', () => {
+		res.status(200).send(data);
+	})
+});
+
+router.get('/:name', (req, res, next) => {
+	fs.appendFile(filePath, req.params.name + '\n', () => {
+		res.status(200).send({ success: true });
+	});
+});
+
+module.exports = router;
+
